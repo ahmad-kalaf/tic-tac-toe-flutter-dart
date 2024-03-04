5a02420b65cebde7cc321c50dd227bfc8adbe333
Ahmad Kalaf <ahmad.it@outlook.de>
2024-03-04 15:30:04 +0100

    Implemented new version of GUI and physics of switching player and tap on game fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:tic_tac_toe_game/fields_row.dart';
 
 void main() {
   runApp(const TicTacToeGame());
@@ -12,48 +10,109 @@ class TicTacToeGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      title: 'Tic Tac Toe',
+      home: GameScreen(),
       debugShowCheckedModeBanner: false,
-      home: MainScreen(),
     );
   }
 }
 
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
+class GameScreen extends StatefulWidget {
+  const GameScreen({super.key});
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
+  State<GameScreen> createState() => _GameScreenState();
 }
 
-class _MainScreenState extends State<MainScreen> {
-  String currentPlayer = "X IST DRAN";
-  String winner = "";
-  bool allFiledsFull = false;
+class _GameScreenState extends State<GameScreen> {
+  int currPlayer = 1;
+  List<String> player = List.generate(9, (index) => ' ');
+  var keys = <int>[0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+  get kButtonStyle =>
+      const ButtonStyle(
+        backgroundColor: MaterialStatePropertyAll(Colors.greenAccent),
+        enableFeedback: false,
+        overlayColor: MaterialStatePropertyAll(Colors.red),
+      );
+
+  void switchPlayer(int num) {
+    setState(() {
+      player[num] = currPlayer % 2 == 0 ? 'O' : 'X';
+      currPlayer++;
+    });
+  }
+
+  bool disableButton(int buttonKey) {
+    return (player[buttonKey] == 'O' || player[buttonKey] == 'X')
+        ? true
+        : false;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      /*appBar: AppBar(
-        title: Text(
-          currentPlayer,
-          style: GoogleFonts.getFont(
-            "Bebas Neue",
-            textStyle: const TextStyle(
-              fontSize: 30,
-            ),
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: Text("Tic Tac Toe"),
         centerTitle: true,
-      ),*/
+      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          // info-text to show whos turn it is
-          Text("X / O ist dran"),
-          // 9 game fileds
-          FieldsRow(),
-          FieldsRow(),
-          FieldsRow(),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Text(
+              currPlayer % 2 == 0 ? 'O ist dran' : 'X ist dran',
+              style: const TextStyle(
+                fontSize: 40,
+              ),
+            ),
+          ),
+          Flexible(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3),
+              itemCount: 9,
+              shrinkWrap: true,
+              itemBuilder: (context, index) =>
+                  Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: ElevatedButton(
+                      onPressed: disableButton(index)
+                          ? null
+                          : () {
+                        switchPlayer(index);
+                      },
+                      style: kButtonStyle,
+                      child: Text(
+                        player[keys[index]],
+                        style: Theme
+                            .of(context)
+                            .textTheme
+                            .headlineMedium,
+                      ),
+                    ),
+                  ),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                currPlayer = 1;
+                player = List.generate(9, (index) => ' ');
+                keys = <int>[0, 1, 2, 3, 4, 5, 6, 7, 8];
+              });
+            },
+            style: kButtonStyle,
+            child: const Text(
+              'Neustarten',
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.black,
+              ),
+            ),
+          ),
         ],
       ),
     );
